bb08c2c17c9ef773ed3c243ed38d56b9762dc562
Jaime Martinez <jmslbam@gmail.com>
2021-04-20 21:16:14 +0200

    Build and bump version

--- distributor-meta-data.php
+++ distributor-meta-data.php
@@ -3,12 +3,12 @@
  * Plugin Name: Distributor Meta Data
  * Plugin URI: https://github.com/burovoordeboeg/distributor-meta-data
  * Description: Push and replace related post_id's in meta data and Gutenberg content blocks. 
- * Version: 0.2.0
+ * Version: 0.3.0
  * Author: Buro voor de Boeg
  * Author URI: https://www.burovoordeboeg.nl
  * License: MIT
- * Requires PHP: 7.3
- * Tested up to: 5.6
+ * Requires PHP: 7.2
+ * Tested up to: 5.7.1
  * Test up to Distributor: 1.6.2
  */
 
